0c59eee2d1f58826ea727a2465177b59acfc31dc
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-10-10 11:36:27 -0300

    Creado modelo tweet y user

---
+++ Model/Models/Tweet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+    public class Tweet
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public int UserId { get; set; }
+        [Required]
+        public string Type { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public int ParentId { get; set; }
+        public User User { get; set; }
+
+    }
+}
