4bff56a5b74bbbe0a97bbdf25278014cce92b9ec
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-10-10 14:41:52 -0300

    CRUD para tweet

--- API/Controllers/TweetsController.cs
+++ API/Controllers/TweetsController.cs
@@ -9,24 +9,40 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using Model.Models;
+using API.Functions;
 
 namespace API.Controllers
 {
     public class TweetsController : ApiController
     {
-        private UserTweetContext db = new UserTweetContext();
+        private TweetFunctions _tweet = new TweetFunctions();
 
         // GET: api/Tweets
-        public IQueryable<Tweet> GetTweets()
+        [HttpGet]
+        [Route("api/tweets")]
+        public IQueryable<UserTweet> GetMainTweets()
         {
-            return db.Tweets;
+            return _tweet.GetAllMainTweets();
+        }
+
+        [HttpGet]
+        [Route("api/tweets/comments/{id}")]
+        public IHttpActionResult GetComments(int id)
+        {
+            var tweets = _tweet.GetAllComments(id);
+            if(tweets.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(tweets);
         }
 
         // GET: api/Tweets/5
-        [ResponseType(typeof(Tweet))]
+        [HttpGet]
+        [Route("api/tweets/{id}")]
         public IHttpActionResult GetTweet(int id)
         {
-            Tweet tweet = db.Tweets.Find(id);
+            var tweet = _tweet.GetTweetById(id);
             if (tweet == null)
             {
                 return NotFound();
@@ -35,84 +51,155 @@ namespace API.Controllers
             return Ok(tweet);
         }
 
-        // PUT: api/Tweets/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutTweet(int id, Tweet tweet)
+        [HttpPost]
+        [Route("api/tweets/create")]
+        public HttpResponseMessage Create(Tweet tweet)
         {
-            if (!ModelState.IsValid)
+            if(string.IsNullOrWhiteSpace(tweet.Content))
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Tweet cannot be empty");
+            }
+            else
+            {
+                try
+                {
+                    var tweetFunction = new TweetFunctions();
+                    tweetFunction.CreateTweet(tweet);
+                    var mesg = Request.CreateResponse(HttpStatusCode.Created);
+                    mesg.Headers.Location = new Uri(Request.RequestUri + tweet.Id.ToString());
+
+                    return mesg;
+                }
+                catch (Exception)
+                {                                 
+                    throw new Exception("Error!!");
+                }
+            }                   
+
+        }
+
+        [HttpPost]
+        [Route("api/tweets/edit/{id}")]
+         public IHttpActionResult Edit(int id, Tweet tweet)
+        {
+            if(!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-
-            if (id != tweet.Id)
+            if(id != tweet.Id)
             {
                 return BadRequest();
             }
 
-            db.Entry(tweet).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TweetExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            tweet.UpdateDate = DateTime.Now;
+            var tweetUpdate = new TweetFunctions();
+            var result = tweetUpdate.EditTweet(tweet);
 
             return StatusCode(HttpStatusCode.NoContent);
         }
 
-        // POST: api/Tweets
-        [ResponseType(typeof(Tweet))]
-        public IHttpActionResult PostTweet(Tweet tweet)
+        public IHttpActionResult Delete(int tweetId, int userId)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.Tweets.Add(tweet);
-            db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = tweet.Id }, tweet);
-        }
-
-        // DELETE: api/Tweets/5
-        [ResponseType(typeof(Tweet))]
-        public IHttpActionResult DeleteTweet(int id)
-        {
-            Tweet tweet = db.Tweets.Find(id);
-            if (tweet == null)
+            var tweet = _tweet.GetTweetById(tweetId);
+            if(tweet == null)
             {
                 return NotFound();
             }
-
-            db.Tweets.Remove(tweet);
-            db.SaveChanges();
-
-            return Ok(tweet);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
+            else
             {
-                db.Dispose();
+                try
+                {
+                    _tweet.DeleteTweet(tweetId, userId);
+                    return Ok(tweet);
+
+                }
+                catch (Exception ex)
+                {
+                    return InternalServerError(ex);
+                }
             }
-            base.Dispose(disposing);
+            
+
         }
 
-        private bool TweetExists(int id)
-        {
-            return db.Tweets.Count(e => e.Id == id) > 0;
-        }
+        //// PUT: api/Tweets/5
+        //[ResponseType(typeof(void))]
+        //public IHttpActionResult PutTweet(int id, Tweet tweet)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    if (id != tweet.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    db.Entry(tweet).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!TweetExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
+
+        //// POST: api/Tweets
+        //[ResponseType(typeof(Tweet))]
+        //public IHttpActionResult PostTweet(Tweet tweet)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    db.Tweets.Add(tweet);
+        //    db.SaveChanges();
+
+        //    return CreatedAtRoute("DefaultApi", new { id = tweet.Id }, tweet);
+        //}
+
+        //// DELETE: api/Tweets/5
+        //[ResponseType(typeof(Tweet))]
+        //public IHttpActionResult DeleteTweet(int id)
+        //{
+        //    Tweet tweet = db.Tweets.Find(id);
+        //    if (tweet == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    db.Tweets.Remove(tweet);
+        //    db.SaveChanges();
+
+        //    return Ok(tweet);
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+
+        //private bool TweetExists(int id)
+        //{
+        //    return db.Tweets.Count(e => e.Id == id) > 0;
+        //}
     }
 }
--- API/Functions/TweetFunctions.cs
+++ API/Functions/TweetFunctions.cs
@@ -1,6 +1,8 @@
 ﻿using Model.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 
@@ -10,20 +12,18 @@ namespace API.Functions
     {
         private UserTweetContext db = new UserTweetContext();
 
-        public IQueryable<UserTweet> GetAllTweets()
+        public IQueryable<UserTweet> GetAllMainTweets()
         {
             //Se hace join para integrar datos para dar uso a esta viewmodel
             var data = from t in db.Tweets
-                       join
-                 u in db.Users on t.UserId equals u.Id
+                       join u in db.Users on t.UserId equals u.Id
+                       where t.ParentId == null
                        select new UserTweet
                        {
                            FirstName = u.FirstName,
                            LastName = u.LastName,
                            Content = t.Content,
                            CreateDate = t.CreateDate,
-                           ParentId = t.ParentId,
-                           Password = u.Password,
                            TweetId = t.Id,
                            Type = t.Type,
                            UpdateDate = t.UpdateDate,
@@ -32,6 +32,90 @@ namespace API.Functions
 
                        };
             return data;
+        }
+
+        public IQueryable<UserTweet> GetAllComments(int id)
+        {
+            //Se hace join para integrar datos para dar uso a esta viewmodel
+            var data = from t in db.Tweets
+                       join u in db.Users on t.UserId equals u.Id
+                       where t.ParentId == id
+                       select new UserTweet
+                       {
+                           FirstName = u.FirstName,
+                           LastName = u.LastName,
+                           Content = t.Content,
+                           CreateDate = t.CreateDate,
+                           TweetId = t.Id,
+                           Type = t.Type,
+                           UpdateDate = t.UpdateDate,
+                           UserId = t.UserId,
+                           Username = u.Username,
+
+                       };
+            return data;
+        }
+
+        public void CreateTweet(Tweet tweet)
+        {
+            if(tweet == null)
+            {
+                throw new NotImplementedException("Tweet not initializaed");
+
+            }
+            else
+            {
+                tweet.CreateDate = DateTime.Now;
+                db.Tweets.Add(tweet);
+                db.SaveChanges();
+            }
+        }
+
+        public UserTweet GetTweetById(int id)
+        {
+            //Se hace join para integrar datos para dar uso a esta viewmodel
+            var data = from t in db.Tweets
+                       join u in db.Users on t.UserId equals u.Id
+                       where t.ParentId == null && t.Id == id
+                       select new UserTweet
+                       {
+                           FirstName = u.FirstName,
+                           LastName = u.LastName,
+                           Content = t.Content,
+                           CreateDate = t.CreateDate,
+                           TweetId = t.Id,
+                           Type = t.Type,
+                           UpdateDate = t.UpdateDate,
+                           UserId = t.UserId,
+                           Username = u.Username,
+
+                       };
+            return data.SingleOrDefault();
+        }
+
+        public void DeleteTweet(int tweetId, int userId)
+        {
+            var tweetComments = db.Tweets.Where(t => t.ParentId == tweetId).ToList();
+            if(tweetComments.Count > 0)
+            {
+                for (int i = 0; i < tweetComments.Count; i++)
+                {
+                    db.Tweets.Remove(tweetComments[i]);
+                }
+                db.SaveChanges();
+                 //Borrar 2 id de  la tabla
+                var tweetEntity = db.Tweets.Where(t => t.Id == tweetId && t.UserId == userId).SingleOrDefault();
+                db.Tweets.Remove(tweetEntity);
+                db.SaveChanges();
+            }
+        }
+
+        public bool EditTweet(Tweet tweet)
+        {
+            db.Entry(tweet).State = EntityState.Modified; 
+            db.SaveChanges();
+            return true;
+            
         }
     }
 }
